dece4fd9e8196e75fa95d13ecddfea038ce43e1c
Precious Akams <preciousakams@yahoo.com>
2022-06-01 17:13:25 +0100

    style updated

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -17,23 +17,23 @@ class Calculator extends React.Component {
             <div className="ac">AC</div>
             <div className="plusMinus">+/-</div>
             <div className="percent">%</div>
-            <div className="divide">/</div>
+            <div className="divide orange">/</div>
             <div className="seven">7</div>
             <div className="eight">8</div>
             <div className="nine">9</div>
-            <div className="multiply">x</div>
+            <div className="multiply orange">x</div>
             <div className="four">4</div>
             <div className="five">5</div>
             <div className="six">6</div>
-            <div className="minus">-</div>
+            <div className="minus orange">-</div>
             <div className="one">1</div>
             <div className="two">2</div>
             <div className="three">3</div>
-            <div className="plus">+</div>
+            <div className="plus orange">+</div>
             <div className="zero">0</div>
             <div className="zero" />
             <div className="dot">.</div>
-            <div className="equalTo">=</div>
+            <div className="equalTo orange">=</div>
           </div>
         </div>
       </div>
