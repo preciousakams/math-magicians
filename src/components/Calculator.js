8a2c71fb68b71db3cfda127e764906a05e52a161
Precious Akams <preciousakams@yahoo.com>
2022-06-02 17:13:33 +0100

    .

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,55 +1,43 @@
-import React, { useState } from 'react';
-import calculate from '../logic/calculate';
+import React from 'react';
 
-function Calculator() {
-  const [data, setData] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  const handleCalc = async (e) => {
-    const response = await calculate(data, e.target.name);
-    setData({
-      total: response.total,
-      next: response.next,
-      operation: response.operation,
-    });
-  };
-
-  const { total, next } = data;
-  return (
-    <div className="container">
-      <div className="calc">
-        <div className="display">
-          <h1 className="displayCalc">
-            {next || total || 0}
-          </h1>
-        </div>
-        <div className="grid">
-          <div className="ac"><button type="button" onClick={handleCalc} name="AC">AC</button></div>
-          <div className="plusMinus"><button type="button" onClick={handleCalc} name="+/-">+/-</button></div>
-          <div className="percent"><button type="button" onClick={handleCalc} name="%">%</button></div>
-          <div className="divide orange"><button type="button" onClick={handleCalc} name="/">/</button></div>
-          <div className="seven"><button type="button" onClick={handleCalc} name="7">7</button></div>
-          <div className="eight"><button type="button" onClick={handleCalc} name="8">8</button></div>
-          <div className="nine"><button type="button" onClick={handleCalc} name="9">9</button></div>
-          <div className="multiply orange"><button type="button" onClick={handleCalc} name="x">x</button></div>
-          <div className="four"><button type="button" onClick={handleCalc} name="4">4</button></div>
-          <div className="five"><button type="button" onClick={handleCalc} name="5">5</button></div>
-          <div className="six"><button type="button" onClick={handleCalc} name="6">6</button></div>
-          <div className="minus orange"><button type="button" onClick={handleCalc} name="-">-</button></div>
-          <div className="one"><button type="button" onClick={handleCalc} name="1">1</button></div>
-          <div className="two"><button type="button" onClick={handleCalc} name="2">2</button></div>
-          <div className="three"><button type="button" onClick={handleCalc} name="3">3</button></div>
-          <div className="plus orange"><button type="button" onClick={handleCalc} name="+">+</button></div>
-          <div className="zero"><button type="button" onClick={handleCalc} name="0">0</button></div>
-          <div className="zero"><button type="button" onClick={handleCalc} name="0" className="none">0</button></div>
-          <div className="dot"><button type="button" onClick={handleCalc} name=".">.</button></div>
-          <div className="equalTo orange"><button type="button" onClick={handleCalc} name="=">=</button></div>
+  render() {
+    return (
+      <div className="container">
+        <div className="calc">
+          <div className="display">
+            <h1 className="displayCalc">0</h1>
+          </div>
+          <div className="grid">
+            <div className="ac">AC</div>
+            <div className="plusMinus">+/-</div>
+            <div className="percent">%</div>
+            <div className="divide orange">/</div>
+            <div className="seven">7</div>
+            <div className="eight">8</div>
+            <div className="nine">9</div>
+            <div className="multiply orange">x</div>
+            <div className="four">4</div>
+            <div className="five">5</div>
+            <div className="six">6</div>
+            <div className="minus orange">-</div>
+            <div className="one">1</div>
+            <div className="two">2</div>
+            <div className="three">3</div>
+            <div className="plus orange">+</div>
+            <div className="zero">0</div>
+            <div className="zero" />
+            <div className="dot">.</div>
+            <div className="equalTo orange">=</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default Calculator;
