72246b3a56b769cdb285bd5bf0ce5ebe550916cd
Precious Akams <preciousakams@yahoo.com>
2022-06-09 14:20:28 +0100

    newPages added

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="homeDiv">
+    <h2>
+      Welcome to our page!
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Morbi maximus et risus id tristique. Quisque aliquam ultricies
+      commodo. Class aptent taciti sociosqu ad litora torquent per
+      conubia nostra, per inceptos himenaeos. Vestibulum eget quam
+      nec odio interdum dictum vel sed felis.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Morbi maximus et risus id tristique. Quisque aliquam ultricies
+      commodo. Class aptent taciti sociosqu ad litora torquent per
+      conubia nostra, per inceptos himenaeos. Vestibulum eget quam
+      nec odio interdum dictum vel sed felis.
+    </p>
+  </div>
+);
+
+export default Home;
